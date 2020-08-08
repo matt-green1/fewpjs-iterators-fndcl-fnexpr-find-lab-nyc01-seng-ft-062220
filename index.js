424e0c7ae545478c1be3604107a3c1368c133c58
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2020-08-08 12:50:31 -0400

    Done.

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 
 const testVar = {}
 
+// const record = [
+//   {year: "2018", result: "L"},
+//   {year: "2017", result: "W"},
+//   {year: "2016", result: "N/A"}
+// ]
+
 function testFunc() {
   return "hi"
 }
@@ -11,10 +17,12 @@ function superbowlWin(arrayOfObjs) {
     let win = arrayOfObjs.find(function(obj) {
       return obj.result === "W"
     })
-    
-    // if(win){ 
-    //   return win.year
-    // } else {
-    //   return undefined
-    // }
+    // return win.year
+    if(win){ 
+      return win.year
+    } else {
+      return undefined
+    }
 }
+
+// console.log(superbowlWin(record))
